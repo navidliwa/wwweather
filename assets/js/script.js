146ec5bea8eee4348a920fb3963669e1fdc41f19
awildivan <75466291+navidliwa@users.noreply.github.com>
2023-01-25 18:32:35 -0700

    edited javascript to add functionality to 5 day forecast tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
                     <h2>${searchedCity}</h2>
                 </div>
                 <div id="wthr-info">
-                    <img src="http://openweathermap.org/img/wn/${currentIcon}@2x.png" class="forecast-icon" />
+                    <img src="http://openweathermap.org/img/wn/${currentIcon}@2x.png" class="forecast-icon" alt="current weather icon" />
                     <h3>Temp: ${currentTemp}°F</h3>
                     <h3>Wind: ${currentWind} MPH</h3>
                     <h3>Humidity: ${currentHumidity}%</h3>
@@ -35,7 +35,35 @@ $(document).ready(function(){
             });
         
         // Fetch 5 day forecast data
-        
+        fetch(forecastUrl)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+                var forecastData = data.list;
+                for(i = 0; i < 40; i += 8) {
+                    var temp = data.list[i].main.temp;
+                    var wind = data.list[i].wind.speed;
+                    var humidity = data.list[i].main.humidity;
+                    var icon = data.list[i].weather[0].icon
+                    var date = data.list[i].dt_txt
+
+                    for(n = 1; n < 6; n++) {
+                        document.getElementById('tab' + n).innerHTML = date
+                        document.getElementById('day' + n).innerHTML =
+                        `<div>
+                            <h2>${searchedCity}</h2>
+                        </div>
+                        <div id="future-info">
+                            <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="forecast-icon" alt="forecast weather icon" />
+                            <h3>Temp: ${temp}°F</h3>
+                            <h3>Wind: ${wind} MPH</h3>
+                            <h3>Humididty: ${humidity}%</h3>
+                        </div>`;
+                    }
+                }
+            });
     })
 
 

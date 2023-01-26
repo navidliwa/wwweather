c4915ddc9c20bbc9bdd473bfdc1a782c3b6d6be0
awildivan <75466291+navidliwa@users.noreply.github.com>
2023-01-25 22:18:05 -0700

    fixed search button and 5 day forecast functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,24 @@ $(document).ready(function(){
     // Enables tab functionality for Materialize CSS framework
     $('.tabs').tabs();
 
-    // Get searched city from input field
-    var searchedCity = $("#city-search").val();
-    // API url for current weather data
-    var currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=a09ff729a95cbb2afe677c333b6cde65&&units=imperial`;
-    // API url for 5 day forecast
-    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchedCity}&appid=a09ff729a95cbb2afe677c333b6cde65&&units=imperial`;
+    var searchedCity;
+    var recentSearch = [];
 
+    // Get searched city from input field
+    function getSearchedCity () {
+       return $("#city-search").val();
+    }
+    
+    // Fill out page with searched city weather data
     $("#searchBtn").on('click', function(event) {
+        searchedCity = getSearchedCity();
+        // API url for current weather data
+        var currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=a09ff729a95cbb2afe677c333b6cde65&&units=imperial`;
+        // API url for 5 day forecast
+        var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchedCity}&appid=a09ff729a95cbb2afe677c333b6cde65&&units=imperial`;
         // Fetch current weather data
         fetch(currentUrl)
-            .then(function(response) {
+        .then(function(response) {
                 return response.json();
             })
             .then(function(data) {
@@ -21,7 +28,7 @@ $(document).ready(function(){
                 var currentIcon = data.weather[0].icon;
                 var currentHumidity = data.main.humidity;
                 var currentWind = data.wind.speed;
-                // Add weather data to DOM
+                // Add weather data to page
                 document.getElementById('current-weather').innerHTML = 
                 `<div>
                     <h2>${searchedCity}</h2>
@@ -42,16 +49,69 @@ $(document).ready(function(){
             .then(function(data) {
                 console.log(data);
                 var forecastData = data.list;
-                for(i = 0; i < 40; i += 8) {
+                for(i = 0; i < 40; i+=8) {
                     var temp = data.list[i].main.temp;
                     var wind = data.list[i].wind.speed;
                     var humidity = data.list[i].main.humidity;
                     var icon = data.list[i].weather[0].icon
                     var date = data.list[i].dt_txt
-
-                    for(n = 1; n < 6; n++) {
-                        document.getElementById('tab' + n).innerHTML = date
-                        document.getElementById('day' + n).innerHTML =
+                    console.log("------------", date);
+                    // Add forecast data to page
+                    if (i === 0) {
+                        document.getElementById('tab1').innerHTML = date
+                        document.getElementById('day1').innerHTML =
+                        `<div>
+                            <h2>${searchedCity}</h2>
+                        </div>
+                        <div id="future-info">
+                            <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="forecast-icon" alt="forecast weather icon" />
+                            <h3>Temp: ${temp}°F</h3>
+                            <h3>Wind: ${wind} MPH</h3>
+                            <h3>Humididty: ${humidity}%</h3>
+                        </div>`;
+                    } 
+                    if (i === 8) {
+                        document.getElementById('tab2').innerHTML = date
+                        document.getElementById('day2').innerHTML =
+                        `<div>
+                            <h2>${searchedCity}</h2>
+                        </div>
+                        <div id="future-info">
+                            <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="forecast-icon" alt="forecast weather icon" />
+                            <h3>Temp: ${temp}°F</h3>
+                            <h3>Wind: ${wind} MPH</h3>
+                            <h3>Humididty: ${humidity}%</h3>
+                        </div>`;
+                    } 
+                    if (i === 16) {
+                        document.getElementById('tab3').innerHTML = date
+                        document.getElementById('day3').innerHTML =
+                        `<div>
+                            <h2>${searchedCity}</h2>
+                        </div>
+                        <div id="future-info">
+                            <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="forecast-icon" alt="forecast weather icon" />
+                            <h3>Temp: ${temp}°F</h3>
+                            <h3>Wind: ${wind} MPH</h3>
+                            <h3>Humididty: ${humidity}%</h3>
+                        </div>`;
+                    } 
+                    if (i === 24) {
+                        document.getElementById('tab4').innerHTML = date
+                        document.getElementById('day4').innerHTML =
+                        `<div>
+                            <h2>${searchedCity}</h2>
+                        </div>
+                        <div id="future-info">
+                            <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="forecast-icon" alt="forecast weather icon" />
+                            <h3>Temp: ${temp}°F</h3>
+                            <h3>Wind: ${wind} MPH</h3>
+                            <h3>Humididty: ${humidity}%</h3>
+                        </div>`;
+                    } 
+                    if (i === 32) {
+                        document.getElementById('tab5').innerHTML = date
+                        document.getElementById('day5').innerHTML =
                         `<div>
                             <h2>${searchedCity}</h2>
                         </div>
@@ -64,7 +124,7 @@ $(document).ready(function(){
                     }
                 }
             });
-    })
+    })  
 
 
   });
